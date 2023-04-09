232034d2d8390717f810bd8aaf84a4071995852f
extremety1989 <andreazure1989@hotmail.fr>
2023-04-09 17:38:56 +0200

    del

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from object_detection import YOLOv7
-from utils.detections import draw
-import json
 import cv2
-
-yolov7 = YOLOv7()
-yolov7.load('coco.weights', classes='coco.yaml', device='cpu') # use 'gpu' for CUDA GPU inference
 
 webcam = cv2.VideoCapture(0)
 
@@ -15,10 +9,6 @@ try:
     while webcam.isOpened():
         ret, frame = webcam.read()
         if ret == True:
-            detections = yolov7.detect(frame, track=True)
-            detected_frame = draw(frame, detections)
-            print(json.dumps(detections, indent=4))
-            cv2.imshow('webcam', detected_frame)
             cv2.waitKey(1)
         else:
             break
@@ -26,5 +16,3 @@ except KeyboardInterrupt:
     pass
 
 webcam.release()
-print('[+] webcam closed')
-yolov7.unload()
